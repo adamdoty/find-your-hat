3378c951cfa5c8cfe74456fcf36ccc45edfee585
Adam Doty <acdoty15@gmail.com>
2023-03-10 10:37:33 -0800

    Add command line arguments to set the field size

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const { Field } = require("./field");
 const prompt = require("prompt-sync")({ sigint: true });
+const fieldWidth = 5;
+const fieldHeight = 4;
+const percentageOfHoles = 20;
 
 let isRunning = true;
 
-const myField = new Field(Field.generateField(4, 5, 20));
+let fieldGrid;
+
+if (process.argv.length > 2) {
+  fieldGrid = Field.generateField(
+    process.argv[2],
+    process.argv[3],
+    process.argv[4]
+  );
+} else {
+  fieldGrid = Field.generateField(fieldHeight, fieldWidth, percentageOfHoles);
+}
+
+const myField = new Field(fieldGrid);
 
 myField.print();
 
